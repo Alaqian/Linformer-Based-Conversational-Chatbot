41257e56a401a1b2bdf0e407963f36900c0dacad
KP <31988529+katsamapol@users.noreply.github.com>
2022-05-15 06:56:41 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,11 +14,19 @@ def main():
 	parser = argparse.ArgumentParser(description='Create Chatbot Instance')
 	
 	parser.add_argument('-w', '--weight', default="data_weight", type=str, help='Name to save weights at /saved/weights/<name>')
-	parser.add_argument('-m', '--model', default="transformer", type=str, help='Model: Transformer or Linformer')
+	parser.add_argument('--model', default="linformer", type=str, help='Model: Transformer or Linformer')
+	parser.add_argument('--linear_dimension', default=256, type=int, help='Linear dimension of Linformer')
 
 	args = parser.parse_args()
-	
-	opt = Options(batchsize=32, device=torch.device("cpu"), epochs=200, lr=0.01, max_len = 32, save_path = f'saved/weights/{args.weight}')
+
+	if(args.model.lower() == "transformer"):
+		max_len = 32
+	elif(args.model.lower() == "linformer"):
+		max_len = args.linear_dimension
+	else:
+		print("Please choose a model between \"Transformer\" and \"Linformer\"")
+		quit()
+	opt = Options(batchsize=32, device=torch.device("cpu"), epochs=200, lr=0.01, max_len = max_len, save_path = f'saved/weights/{args.weight}')
 	data_iter, infield, outfield, opt = json2datatools(path = f'saved/data/data2_train_9010.json', opt=opt)
 	emb_dim, n_layers, heads, dropout = 512, 6, 8, 0.1 
 
@@ -38,10 +46,11 @@ def main():
 		else:
 			model_input = your_input
 		bot_reply = talk(model_input, model, opt, infield, outfield)
-		if ("bye" in your_input or "bye ttyl" in bot_reply):
-			print('Bot > '+ bot_reply + '\n')
-			break
-		else:
-			print('Bot > '+ bot_reply + '\n') 
+		# if ("bye" in your_input or "bye ttyl" in bot_reply):
+		# 	print('Bot > '+ bot_reply + '\n')
+		# 	break
+		# else:
+		# 	print('Bot > '+ bot_reply + '\n') 
+		print('Bot > '+ bot_reply + '\n') 
 if __name__ == "__main__":
 	main()
